484b447be72279f947c1e4ddd4e42fbada7d537a
esievaughn <78444940+esievaughn@users.noreply.github.com>
2021-02-14 12:14:31 -0800

    Add files via upload
    --fixed error by adding missing "s" in "Elements" of document.getElementsByClassName
    --added additional CSS styling

--- project9-countdown-slideshow/JS/main.js
+++ project9-countdown-slideshow/JS/main.js
@@ -33,8 +33,8 @@ function currentSlide(n) {
 
 function showSlides(n) {
     var i;
-    var slides= document.getElementByClassName("mySlides fade");
-    var dots= document.getElementByClassName("dot");
+    var slides= document.getElementsByClassName("mySlides");
+    var dots= document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex= slides.length}
     for (i = 0; i < slides.length; i++) {
