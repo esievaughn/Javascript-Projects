16b3475165a57b0bff3dc395ca39bd4328f7c357
esievaughn <78444940+esievaughn@users.noreply.github.com>
2021-02-14 12:13:11 -0800

    Add files via upload
    --fixed error by adding missing "s" in "Elements" of document.getElementsByClassName
    --added additional CSS styling

---
+++ project9-countdown-slideshow/project9-countdown-slideshow/JS/main.js
@@ -0,0 +1,48 @@
+
+
+
+
+//COUNTDOWN BY SECONDS//
+function countdown() {
+    var seconds= document.getElementById("seconds").value;
+
+    function tick() {
+        seconds= seconds-1;
+        timer.innnerHTML= seconds;
+        setTimeout(tick, 1000);         //setTimeout--means the program is pausing for 1000 milliseconds or 1 second)//
+    if (seconds == -1) {
+        alert("Time's up!");   //alert show after seconds input runs out//
+    }
+        }
+    tick();
+}
+
+//SLIDESHOW//
+var slideIndex = 1;
+    showSlides(slideIndex);
+
+//NEXT & PREVIOUS CONTROLS//
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+//THUMBNAIL IMG CONTROLS//
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides= document.getElementsByClassName("mySlides");
+    var dots= document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex= slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display= "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className= dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display= "block";
+    dots[slideIndex-1].className += " active";
+}
