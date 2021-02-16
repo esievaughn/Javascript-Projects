74dc9f87a0aa49d67c497c48e021e1cd21bbb031
esievaughn <78444940+esievaughn@users.noreply.github.com>
2021-02-16 12:47:01 -0800

    Add files via upload
    edited line 161 "x" to "y" for 2, 4, 6 line cross through

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -158,7 +158,7 @@ function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
         //this condition is similar to the one above. It was necessary for 6, 4, ,2 win conditions//
         if (x1 <= x2 && y1 >= y2) {
             if (x < x2) {x += 10;}
-            if (y > y2) {x -= 10;}
+            if (y > y2) {y -= 10;}
             if (x >= x2 && y <= y2) {cancelAnimationFrame(animationLoop);}
         }
     }
